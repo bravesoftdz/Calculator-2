099ec41f00d1ce4141fee5e9c16afaa6b3dd1c97
Ryan Potts <rpottsoh@gmail.com>
2017-07-18 00:45:17 -0400

    implemented class TBridgeUI
    TBridgeUI acts as a bridge between the test runner and the students solution.  The student will need to fill in information regarding their form into the TBridgeUI Constructor, function errMsg, and function DisplayedResult.

--- uCalculatorTests.pas
+++ uCalculatorTests.pas
@@ -2,19 +2,38 @@ unit uCalculatorTests;
 
 interface
 uses
-  DUnitX.TestFramework, uCalculatorUI;
+  System.SysUtils,
+  System.Generics.Collections,
+  DUnitX.TestFramework,
+  uCalculatorUI;
 
 type
+  IBridgeUI = Interface(IInvokable)
+    ['{A64CAEF2-EE61-415D-80F7-F02C3B033133}']
+    procedure SimClick(aButton: string); overload;
+    procedure SimClick(aButton: integer); overload;
+    function DisplayedResult: string;
+    function errMsg: string;
+  End;
 
+  TBridgeUI = class(TInterfacedObject, IBridgeUI)
+  private
+    fFormButtons: TDictionary<string, string>;
+    FCalculatorUI: TfrmCalculator;
+  public
+    constructor Create;
+    destructor Destroy; override;
+    procedure SimClick(aButton: string); overload;
+    procedure SimClick(aButton: integer); overload;
+    function DisplayedResult: string;
+    function errMsg: string;
+  end;
+
+  {$M+}
   [TestFixture]
   TCalculatorUITests = class(TObject)
   private
-    FCalculatorUI: TfrmCalculator;
-  public
-    [Setup]
-    procedure Setup;
-    [TearDown]
-    procedure TearDown;
+    FBridgeUI: IBridgeUI;
   published
 //    [Ignore('Comment the "[Ignore]" statement to run the test')]
     procedure DisplayEqualsAllTenDigitsOneTime;
@@ -41,13 +60,13 @@ type
     procedure DisplayOnePlusOneEqualsTwo;
 
     [Ignore]
+    procedure DisplaySeventyFiveMinusTwelveEqualsSixtyThree;
+
+    [Ignore]
     procedure DisplayTwoTimesSevenEqualsFourteen;
 
     [Ignore]
     procedure DisplayTwelveDividedByThreeEqualsFour;
-
-    [Ignore]
-    procedure DisplaySeventyFiveMinusTwelveEqualsSixtyThree;
 
     [Ignore]
     procedure PerformTwoStepCalculation;
@@ -61,193 +80,305 @@ type
     [Ignore]
     procedure RecoverFromDivideByZero;
   end;
+  {$M-}
 
 implementation
-uses VCL.Forms;
+uses System.Classes, VCL.StdCtrls;
 
-procedure TCalculatorUITests.Setup;
+{ TBridgeUI }
+
+constructor TBridgeUI.Create;
+var i: integer;
 begin
-  FCalculatorUI := TfrmCalculator.Create(nil);
+  inherited create;
+  fCalculatorUI := TfrmCalculator.Create(nil);
+  fFormButtons := TDictionary<string, string>.Create;
+
+  //             Operation  Name of button
+  fFormButtons.Add('sign',  'btnSign');               // ± button
+  fFormButtons.Add('+',     'btnPlus');               // + button
+  fFormButtons.Add('-',     'btnMinus');              // - button
+  fFormButtons.Add('*',     'btnMultiply');           // * button
+  fFormButtons.Add('/',     'btnDivide');             // / button
+  fFormButtons.Add('back',  'btnBackSpace');          // back button
+  fFormButtons.Add('clear', 'btnClr');                // clear button
+  fFormButtons.Add('=',     'btnEquals');             // = button
+  fFormButtons.Add('.',     'btnDecimal');            // . button
+  for i := 0 to 9 do
+    fFormButtons.Add(i.ToString, 'btn' + i.ToString); // 0..9 buttons
 end;
 
-procedure TCalculatorUITests.TearDown;
-begin
-  FCalculatorUI.Free;
+function TBridgeUI.DisplayedResult: string;
+begin    //result should equal string output of component that displays results
+  result := fCalculatorUI.edtDisplay.Text;
 end;
 
+function TBridgeUI.errMsg;
+begin  //result should equal a string or method that returns an error message that is displayed when an error occurs
+       //  ie. dividing by zero
+  result := fCalculatorUI.errMsg;
+end;
+
+{$region 'TBridgeUI Implementation'}
+procedure TBridgeUI.SimClick(aButton: integer);
+begin
+  SimClick(aButton.ToString);
+end;
+
+procedure TBridgeUI.SimClick(aButton: string);
+var Component: TComponent;
+begin
+  if fFormButtons.ContainsKey(aButton) then
+  begin
+    Component := fCalculatorUI.FindComponent(fFormButtons[aButton]);
+    if assigned(Component) and (Component is TButton) then
+      (Component as TButton).Click;
+  end;
+end;
+
+destructor TBridgeUI.Destroy;
+begin
+  fFormButtons.DisposeOf;
+  fCalculatorUI.DisposeOf;
+  inherited Destroy;
+end;
+{$endregion}
+
+{$region 'TCalculatorUITests'}
 procedure TCalculatorUITests.DisplayEqualsAllTenDigitsOneTime;
 begin
-  FCalculatorUI.btn1.Click;
-  FCalculatorUI.btn2.Click;
-  FCalculatorUI.btn3.Click;
-  FCalculatorUI.btn4.Click;
-  FCalculatorUI.btn5.Click;
-  FCalculatorUI.btn6.Click;
-  FCalculatorUI.btn7.Click;
-  FCalculatorUI.btn8.Click;
-  FCalculatorUI.btn9.Click;
-  FCalculatorUI.btn0.Click;
-  Assert.AreEqual('1234567890',FCalculatorUI.edtDisplay.Text);
+  FBridgeUI := TBridgeUI.Create;
+  with FBridgeUI do
+  begin
+    SimClick(1);
+    SimClick(2);
+    SimClick(3);
+    SimClick(4);
+    SimClick(5);
+    SimClick(6);
+    SimClick(7);
+    SimClick(8);
+    SimClick(9);
+    SimClick(0);
+    Assert.AreEqual('1234567890', DisplayedResult);
+  end;
 end;
 
 procedure TCalculatorUITests.DisplayCorrectValueAfterClearingOldValue;
 begin
-  FCalculatorUI.btn5.Click;
-  FCalculatorUI.btn6.Click;
-  FCalculatorUI.btnDecimal.Click;
-  FCalculatorUI.btn3.Click;
-  FCalculatorUI.btn5.Click;
-  FCalculatorUI.btnSign.Click;
-  FCalculatorUI.btnClr.Click;
+  FBridgeUI := TBridgeUI.Create;
+  with FBridgeUI do
+  begin
+    SimClick(5);
+    SimClick(6);
+    SimClick('.');
+    SimClick(3);
+    SimClick(5);
+    SimClick('sign');
+    SimClick('clear');
 
-  FCalculatorUI.btn1.Click;
-  FCalculatorUI.btn1.Click;
-  FCalculatorUI.btn1.Click;
-  Assert.AreEqual('111',FCalculatorUI.edtDisplay.Text);
+    SimClick(1);
+    SimClick(1);
+    SimClick(1);
+    Assert.AreEqual('111', DisplayedResult);
+  end;
 end;
 
 procedure TCalculatorUITests.DisplayCorrectValueAfterErasingPartOfValue;
 begin
-  FCalculatorUI.btn5.Click;
-  FCalculatorUI.btn6.Click;
-  FCalculatorUI.btnDecimal.Click;
-  FCalculatorUI.btn3.Click;
-  FCalculatorUI.btn5.Click;
-  FCalculatorUI.btnSign.Click;
+  FBridgeUI := TBridgeUI.Create;
+  with FBridgeUI do
+  begin
+    SimClick(5);
+    SimClick(6);
+    SimClick('.');
+    SimClick(3);
+    SimClick(5);
+    SimClick('sign');
 
-  FCalculatorUI.btnBackSpace.Click;
-  FCalculatorUI.btn9.Click;
-  FCalculatorUI.btn5.Click;
-  Assert.AreEqual('-56.395',FCalculatorUI.edtDisplay.Text);
+    SimClick('back');
+    SimClick(9);
+    SimClick(5);
+    Assert.AreEqual('-56.395', DisplayedResult);
+  end;
 end;
 
 procedure TCalculatorUITests.DisplayEqualsEnteredNegativeFloatValue;
 begin
-  FCalculatorUI.btn5.Click;
-  FCalculatorUI.btn6.Click;
-  FCalculatorUI.btnDecimal.Click;
-  FCalculatorUI.btn3.Click;
-  FCalculatorUI.btn5.Click;
-  FCalculatorUI.btnSign.Click;
-  Assert.AreEqual('-56.35',FCalculatorUI.edtDisplay.Text);
+  FBridgeUI := TBridgeUI.Create;
+  with FBridgeUI do
+  begin
+    SimClick(5);
+    SimClick(6);
+    SimClick('.');
+    SimClick(3);
+    SimClick(5);
+    SimClick('sign');
+    Assert.AreEqual('-56.35', DisplayedResult);
+  end;
 end;
 
 procedure TCalculatorUITests.DisplayEqualsEnteredNegativeWholeValue;
 begin
-  FCalculatorUI.btn5.Click;
-  FCalculatorUI.btn6.Click;
-  FCalculatorUI.btnSign.Click;
-  Assert.AreEqual('-56',FCalculatorUI.edtDisplay.Text);
+  FBridgeUI := TBridgeUI.Create;
+  with FBridgeUI do
+  begin
+    SimClick(5);
+    SimClick(6);
+    SimClick('sign');
+    Assert.AreEqual('-56', DisplayedResult);
+  end;
 end;
 
 procedure TCalculatorUITests.DisplayEqualsEnteredPositiveFloatValue;
 begin
-  FCalculatorUI.btn5.Click;
-  FCalculatorUI.btn6.Click;
-  FCalculatorUI.btnDecimal.Click;
-  FCalculatorUI.btn3.Click;
-  FCalculatorUI.btn5.Click;
-  Assert.AreEqual('56.35',FCalculatorUI.edtDisplay.Text);
+  FBridgeUI := TBridgeUI.Create;
+  with FBridgeUI do
+  begin
+    SimClick(5);
+    SimClick(6);
+    SimClick('.');
+    SimClick(3);
+    SimClick(5);
+    Assert.AreEqual('56.35', DisplayedResult);
+  end;
 end;
 
 procedure TCalculatorUITests.DisplayEqualsEnteredPositiveWholeValue;
 begin
-  FCalculatorUI.btn5.Click;
-  FCalculatorUI.btn6.Click;
-  Assert.AreEqual('56',FCalculatorUI.edtDisplay.Text);
+  FBridgeUI := TBridgeUI.Create;
+  with FBridgeUI do
+  begin
+    SimClick(5);
+    SimClick(6);
+    Assert.AreEqual('56', DisplayedResult);
+  end;
 end;
 
 procedure TCalculatorUITests.DisplayOnePlusOneEqualsTwo;
 begin
-  FCalculatorUI.btn1.Click;
-  FCalculatorUI.btnPlus.Click;
-  FCalculatorUI.btn1.Click;
-  FCalculatorUI.btnEquals.Click;
-  Assert.AreEqual('2',FCalculatorUI.edtDisplay.Text);
+  FBridgeUI := TBridgeUI.Create;
+  with FBridgeUI do
+  begin
+    SimClick(1);
+    SimClick('+');
+    SimClick(1);
+    SimClick('=');
+    Assert.AreEqual('2', DisplayedResult);
+  end;
 end;
 
 procedure TCalculatorUITests.DisplaySeventyFiveMinusTwelveEqualsSixtyThree;
 begin
-  FCalculatorUI.btn7.Click;
-  FCalculatorUI.btn5.Click;
-  FCalculatorUI.btnMinus.Click;
-  FCalculatorUI.btn1.Click;
-  FCalculatorUI.btn2.Click;
-  FCalculatorUI.btnEquals.Click;
-  Assert.AreEqual('63',FCalculatorUI.edtDisplay.Text);
+  FBridgeUI := TBridgeUI.Create;
+  with FBridgeUI do
+  begin
+    SimClick(7);
+    SimClick(5);
+    SimClick('-');
+    SimClick(1);
+    SimClick(2);
+    SimClick('=');
+    Assert.AreEqual('63', DisplayedResult);
+  end;
 end;
 
 procedure TCalculatorUITests.DisplayTwelveDividedByThreeEqualsFour;
 begin
-  FCalculatorUI.btn1.Click;
-  FCalculatorUI.btn2.Click;
-  FCalculatorUI.btnDivide.Click;
-  FCalculatorUI.btn3.Click;
-  FCalculatorUI.btnEquals.Click;
-  Assert.AreEqual('4',FCalculatorUI.edtDisplay.Text);
+  FBridgeUI := TBridgeUI.Create;
+  with FBridgeUI do
+  begin
+    SimClick(1);
+    SimClick(2);
+    SimClick('/');
+    SimClick(3);
+    SimClick('=');
+    Assert.AreEqual('4', DisplayedResult);
+  end;
 end;
 
 procedure TCalculatorUITests.DisplayTwoTimesSevenEqualsFourteen;
 begin
-  FCalculatorUI.btn2.Click;
-  FCalculatorUI.btnMultiply.Click;
-  FCalculatorUI.btn7.Click;
-  FCalculatorUI.btnEquals.Click;
-  Assert.AreEqual('14',FCalculatorUI.edtDisplay.Text);
+  FBridgeUI := TBridgeUI.Create;
+  with FBridgeUI do
+  begin
+    SimClick(2);
+    SimClick('*');
+    SimClick(7);
+    SimClick('=');
+    Assert.AreEqual('14', DisplayedResult);
+  end;
 end;
 
 procedure TCalculatorUITests.DividingByZeroIsBad;
 begin
-  FCalculatorUI.btn1.Click;
-  FCalculatorUI.btn2.Click;
-  FCalculatorUI.btnDivide.Click;
-  FCalculatorUI.btn0.Click;
-  FCalculatorUI.btnEquals.Click;
-  Assert.AreEqual(FCalculatorUI.errMsg,FCalculatorUI.edtDisplay.Text);
+  FBridgeUI := TBridgeUI.Create;
+  with FBridgeUI do
+  begin
+    SimClick(1);
+    SimClick(2);
+    SimClick('/');
+    SimClick(0);
+    SimClick('=');
+    Assert.AreEqual(errMsg, DisplayedResult);
+  end;
 end;
 
 procedure TCalculatorUITests.PerformThreeStepCalculation;
 begin
-  FCalculatorUI.btn2.Click;
-  FCalculatorUI.btnMultiply.Click;
-  FCalculatorUI.btn7.Click;
-  FCalculatorUI.btnPlus.Click;
-  FCalculatorUI.btn1.Click;
-  FCalculatorUI.btn2.Click;
-  FCalculatorUI.btnMinus.Click;
-  FCalculatorUI.btn6.Click;
-  FCalculatorUI.btnSign.Click;
-  FCalculatorUI.btnEquals.Click;
-  Assert.AreEqual('32',FCalculatorUI.edtDisplay.Text);
+  FBridgeUI := TBridgeUI.Create;
+  with FBridgeUI do
+  begin
+    SimClick(2);
+    SimClick('*');
+    SimClick(7);
+    SimClick('+');
+    SimClick(1);
+    SimClick(2);
+    SimClick('-');
+    SimClick(6);
+    SimClick('sign');
+    SimClick('=');
+    Assert.AreEqual('32', DisplayedResult);
+  end;
 end;
 
 procedure TCalculatorUITests.PerformTwoStepCalculation;
 begin
-  FCalculatorUI.btn2.Click;
-  FCalculatorUI.btnMultiply.Click;
-  FCalculatorUI.btn7.Click;
-  FCalculatorUI.btnPlus.Click;
-  FCalculatorUI.btn1.Click;
-  FCalculatorUI.btn2.Click;
-  FCalculatorUI.btnEquals.Click;
-  Assert.AreEqual('26',FCalculatorUI.edtDisplay.Text);
+  FBridgeUI := TBridgeUI.Create;
+  with FBridgeUI do
+  begin
+    SimClick(2);
+    SimClick('*');
+    SimClick(7);
+    SimClick('+');
+    SimClick(1);
+    SimClick(2);
+    SimClick('=');
+    Assert.AreEqual('26', DisplayedResult);
+  end;
 end;
 
 procedure TCalculatorUITests.RecoverFromDivideByZero;
 begin
-  FCalculatorUI.btn1.Click;
-  FCalculatorUI.btn2.Click;
-  FCalculatorUI.btnDivide.Click;
-  FCalculatorUI.btn0.Click;
-  FCalculatorUI.btnEquals.Click;
+  FBridgeUI := TBridgeUI.Create;
+  with FBridgeUI do
+  begin
+    SimClick(1);
+    SimClick(2);
+    SimClick('/');
+    SimClick(0);
+    SimClick('=');
 
-  FCalculatorUI.btnClr.Click;
-  FCalculatorUI.btn1.Click;
-  FCalculatorUI.btnPlus.Click;
-  FCalculatorUI.btn1.Click;
-  FCalculatorUI.btnEquals.Click;
-  Assert.AreEqual('2',FCalculatorUI.edtDisplay.Text);
+    SimClick('clear');
+    SimClick(1);
+    SimClick('+');
+    SimClick(1);
+    SimClick('=');
+    Assert.AreEqual('2', DisplayedResult);
+  end;
 end;
+{$endregion}
 
 initialization
   TDUnitX.RegisterTestFixture(TCalculatorUITests);
